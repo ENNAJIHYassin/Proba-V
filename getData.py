733deec9bfaab17888a76f83e5d5cfc11f740e36
ENNAJIHYassin <32954804+ENNAJIHYassin@users.noreply.github.com>
2019-06-07 13:49:28 +0900

    Update getData.py

--- getData.py
+++ getData.py
@@ -1,5 +1,6 @@
 from urllib.request import urlretrieve
 from zipfile import ZipFile
+import os
 
 print("downloading data... please wait")
 urlretrieve('https://kelvins.esa.int/media/competitions/proba-v-super-resolution/probav_data.zip',
@@ -7,3 +8,11 @@ urlretrieve('https://kelvins.esa.int/media/competitions/proba-v-super-resolution
 
 print("extracting data... please wait")
 ZipFile('probav_data.zip').extractall('probav_data/')
+
+# Create directory
+dirName = 'output'
+try:
+    os.mkdir(dirName)
+    print(dirName," Directory Created!") 
+except FileExistsError:
+    print(dirName," Directory already exists!")
